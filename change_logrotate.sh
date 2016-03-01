7f073306f0960ee5f74ce8b2d66375e8056a3585
Sergiy <sergiy_007@ukr.net>
2016-03-01 12:09:28 +0200

    Update change_logrotate.sh

--- change_logrotate.sh
+++ change_logrotate.sh
@@ -5,48 +5,45 @@
 SITE_LOCATION="/home"
 APACHE_GROUP="www-data"
 
-for i in `find $SITE_LOCATION -mindepth 1 -maxdepth 1 -type d -user www-data`
+while SITE_ROOT in "$(find $SITE_LOCATION -mindepth 1 -maxdepth 1 -type d -user www-data)"
 do
 
-SITE_ROOT=$i
-DOMAIN_NAME=`basename $i`
-LOGROTATE_CONFIG="/etc/logrotate.d/apache2-$DOMAIN_NAME"
+  DOMAIN_NAME=$(basename "$SITE_ROOT")
+  LOGROTATE_CONFIG="/etc/logrotate.d/apache2-$DOMAIN_NAME"
 
-
-echo "$SITE_ROOT/logs/error.log {" > $LOGROTATE_CONFIG
-echo '        daily' >> $LOGROTATE_CONFIG  
-echo '        missingok' >> $LOGROTATE_CONFIG
-echo '        rotate 2' >> $LOGROTATE_CONFIG
-echo '        size 10M' >> $LOGROTATE_CONFIG
-echo '        compress' >> $LOGROTATE_CONFIG
-echo '        delaycompress' >> $LOGROTATE_CONFIG
-echo '        notifempty' >> $LOGROTATE_CONFIG
-echo "        create 640 root $APACHE_GROUP" >> $LOGROTATE_CONFIG
-echo '        sharedscripts' >> $LOGROTATE_CONFIG
-echo '        postrotate' >> $LOGROTATE_CONFIG
-echo '                if [ -f "`. /etc/apache2/envvars ; echo ${APACHE_PID_FILE:-/var/run/apache2.pid}`" ]; then' >> $LOGROTATE_CONFIG
-echo '                        /etc/init.d/apache2 reload > /dev/null' >> $LOGROTATE_CONFIG
-echo '                fi' >> $LOGROTATE_CONFIG
-echo '        endscript' >> $LOGROTATE_CONFIG
-echo '}' >> $LOGROTATE_CONFIG
-echo '' >> $LOGROTATE_CONFIG
-
-echo "$SITE_ROOT/logs/access.log {" >> $LOGROTATE_CONFIG
-echo '        daily' >> $LOGROTATE_CONFIG
-echo '        missingok' >> $LOGROTATE_CONFIG
-echo '        rotate 2' >> $LOGROTATE_CONFIG
-echo '        size 10M' >> $LOGROTATE_CONFIG
-echo '        compress' >> $LOGROTATE_CONFIG
-echo '        delaycompress' >> $LOGROTATE_CONFIG
-echo '        notifempty' >> $LOGROTATE_CONFIG
-echo "        create 640 root $APACHE_GROUP" >> $LOGROTATE_CONFIG
-echo '        sharedscripts' >> $LOGROTATE_CONFIG
-echo '        postrotate' >> $LOGROTATE_CONFIG
-echo '                if [ -f "`. /etc/apache2/envvars ; echo ${APACHE_PID_FILE:-/var/run/apache2.pid}`" ]; then' >> $LOGROTATE_CONFIG
-echo '                        /etc/init.d/apache2 reload > /dev/null' >> $LOGROTATE_CONFIG
-echo '                fi' >> $LOGROTATE_CONFIG
-echo '        endscript' >> $LOGROTATE_CONFIG
-echo '}' >> $LOGROTATE_CONFIG
-echo '' >> $LOGROTATE_CONFIG
-
+	echo "$SITE_ROOT/logs/error.log {" > "$LOGROTATE_CONFIG"
+	{
+		echo "        daily"
+		echo "        missingok"
+		echo "        rotate 2"
+		echo "        size 10M"
+		echo "        compress"
+		echo "        delaycompress"
+		echo "        notifempty"
+		echo "        create 640 root $APACHE_GROUP"
+		echo "        sharedscripts"
+		echo "        postrotate"
+		echo '                if [ -f "`. /etc/apache2/envvars ; echo ${APACHE_PID_FILE:-/var/run/apache2.pid}`" ]; then'
+		echo "                        /etc/init.d/apache2 reload > /dev/null"
+		echo "                fi"
+		echo "        endscript"
+		echo "}"
+		echo ""
+		echo "$SITE_ROOT/logs/access.log {"
+		echo "        daily"
+		echo "        missingok"
+		echo "        rotate 2"
+		echo "        size 10M"
+		echo "        compress"
+		echo "        delaycompress"
+		echo "        notifempty"
+		echo "        create 640 root $APACHE_GROUP"
+		echo "        sharedscripts"
+		echo "        postrotate"
+		echo '                if [ -f "`. /etc/apache2/envvars ; echo ${APACHE_PID_FILE:-/var/run/apache2.pid}`" ]; then'
+		echo "                        /etc/init.d/apache2 reload > /dev/null"
+		echo "                fi"
+		echo "        endscript"
+		echo "}"
+	} >> "$LOGROTATE_CONFIG"
 done
