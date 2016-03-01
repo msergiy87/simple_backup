78d8f45d05fe5de0210e9f39d5e964cedd751366
Sergiy <sergiy_007@ukr.net>
2016-03-01 11:31:52 +0200

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,28 +1,33 @@
 #!/bin/bash
 export PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:$PATH
+#set -x
 
 SITE_LOCATION="/home"
 BACKUP_DIR="/data/backups"
-ROTATE="/usr/sbin/logrotate -f"
-DATE=`date +%d-%m-%Y`
 
-case $1 in
+# check conditions
+#------------------------------------------------------------------------------------------
+if [ ! -d "$BACKUP_DIR" ]						# if not equal, not success
+then
+	mkdir -p "$BACKUP_DIR"/daily
+	mkdir -p "$BACKUP_DIR"/monthly_dump_and_data
+fi
+
+case "$1" in
 "daily" )
 
-	for i in `find $SITE_LOCATION -mindepth 1 -maxdepth 1 -type d -user www-data`
+	while SITE_ROOT in "$(find $SITE_LOCATION -mindepth 1 -maxdepth 1 -type d -user www-data)"
 	do
-		SITE_ROOT=$i
-		DOMAIN_NAME=`basename $i`
-
-		tar --numeric-owner -czpf $BACKUP_DIR/daily/$DOMAIN_NAME.tar.gz $SITE_ROOT/public_html
+		DOMAIN_NAME=$(basename "$SITE_ROOT")
+		tar --numeric-owner -czpf "$BACKUP_DIR"/daily/"$DOMAIN_NAME".tar.gz "$SITE_ROOT"/public_html
 	done
 
-	$ROTATE /etc/logrotate_backup_daily.conf
+	logrotate -f /etc/logrotate_backup_daily.conf
 	;;
 
 "monthly" )
-	mv $BACKUP_DIR/daily/*.tar.gz.5 $BACKUP_DIR/monthly_dump_and_data/
-	mv $BACKUP_DIR/daily_mysql_dump/*.sql.gz.5 $BACKUP_DIR/monthly_dump_and_data/
-	$ROTATE /etc/logrotate_backup_monthly.conf
+	mv "$BACKUP_DIR"/daily/*.tar.gz.5 "$BACKUP_DIR"/monthly_dump_and_data/
+	mv "$BACKUP_DIR"/daily_mysql_dump/*.sql.gz.5 "$BACKUP_DIR"/monthly_dump_and_data/
+	logrotate -f /etc/logrotate_backup_monthly.conf
 	;;
 esac
